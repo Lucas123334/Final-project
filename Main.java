88091f98cc147bc4ee5a971f3daee7dc89e27a9b
Lucas123334 <49580@students.ausd.net>
2024-05-21 20:25:31 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,24 @@
+package org.example;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        Scanner input = new Scanner(System.in);
+        System.out.println("---------- Commands ----------");
+        System.out.println("stop game: end");
+        System.out.println("check score: score");
+        while (true){
+            System.out.println("input: ");
+            String decision = input.next();
+            if (decision.equals("score")) game.getScore();
+            else if (decision.equals("end")) {
+                game.getScore();
+                System.exit(0);
+            } else {
+                game.play(decision);
+            }
+        }
+    }
+}
